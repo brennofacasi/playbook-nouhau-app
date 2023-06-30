85482dc3837a09f120aca68c0fb03ae189bf6477
Brenno Cavalcante <brennofaby@gmail.com>
2023-06-30 20:49:22 -0300

    feat: added active class

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import Image from "next/image";
+
 import styles from "./styles.module.scss";
 import joystick from "@/icons/joystick.svg";
 import { links } from "./links";
-import Link from "next/link";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside>
       <div className={styles.logo}>
@@ -12,7 +17,11 @@ const Sidebar = () => {
       </div>
       <nav className={styles.nav}>
         {links.map((link, index) => (
-          <Link key={index} href={link.href}>
+          <Link
+            key={index}
+            href={link.href}
+            className={pathname === link.href ? styles.active : ""}
+          >
             <Image src={link.icon} alt={`Ícone ${link.label}`} />
             {link.label}
           </Link>
