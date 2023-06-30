b8b6991c5512382e6c873e82a59518b6ff20c7d9
Brenno Cavalcante <brennofaby@gmail.com>
2023-06-30 20:50:57 -0300

    feat: added card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { Card } from "@/components/Card";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <>
+      <Card>
+        <h1>
+          Playbook <span className='light highlight'>Nouhau</span>
+        </h1>
+        <section>
+          <h4>Jogos recentes</h4>
+        </section>
+      </Card>
+    </>
+  );
 }
