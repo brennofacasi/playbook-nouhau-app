46b810bdd5f080ba763c2652a0cb8169a72f61d9
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-12 23:49:07 -0300

    feat: added icon and other props

--- src/components/forms/Button/index.tsx
+++ src/components/forms/Button/index.tsx
@@ -1,5 +1,23 @@
+import { ReactElement } from "react";
 import styles from "./styles.module.scss";
+import { ButtonProps } from "./types";
+import Image from "next/image";
 
-export const Button = ({ children }: { children: string }) => {
-  return <button className={styles.button}>{children}</button>;
+export const Button = ({
+  children,
+  primary,
+  icon,
+  center,
+  ...props
+}: ButtonProps): ReactElement => {
+  return (
+    <button
+      className={`${styles.button} ${primary ? styles.primary : ""} ${
+        center ? styles.center : ""
+      }`}
+      {...props}
+    >
+      {icon && <Image src={icon} alt='Ícone' />} {children}
+    </button>
+  );
 };
