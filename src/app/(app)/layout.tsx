d89fc06f4d57e387dd898ee666f63fb6f1bec756
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-12 23:39:54 -0300

    feat: added redirect

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,21 @@
 import Sidebar from "@/components/Sidebar";
+import { getServerSession } from "next-auth";
+import { authOption } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Playbook Nouhau",
   description: "Playbook Nouhau",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOption);
+  if (!session) return redirect("/");
+
   return (
     <main id='wrapper'>
       <Sidebar />
