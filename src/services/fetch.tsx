953840fb759b0b184042111af5463a047468863a
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-12 23:52:39 -0300

    feat: new fetch functions

---
+++ src/services/fetch.tsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export async function fetchApi(path: string, options?: any) {
+  return await fetch(process.env.API_URL + "/" + path, options);
+}
+
+export const clientFetch = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
