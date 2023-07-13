9b8e6f23e8c2142897f9edf7b84b26c18ef80d46
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-12 23:48:04 -0300

    feat: added user image

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,21 @@
+import { useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export const Avatar = ({ name }: { name: string }) => {
+  const { data } = useSession();
   return (
     <div className={styles.avatar__wrapper}>
-      <span className={styles.avatar__name}>{name}</span>
-      <div className={styles.avatar__image}></div>
+      <span className={styles.avatar__name}>{data?.user?.name}</span>
+
+      {data?.user?.image && (
+        <img
+          className={styles.avatar__image}
+          src={data?.user?.image}
+          width={40}
+          height={40}
+          alt='Foto'
+        />
+      )}
     </div>
   );
 };
