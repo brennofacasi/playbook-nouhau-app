100b6a7c4db09a9414bf6488ef6b63e0426cb1dc
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-12 23:49:59 -0300

    del: deleted input component

--- src/components/forms/Input/index.tsx
+++
@@ -1,6 +0,0 @@
-import { InputHTMLAttributes } from "react";
-import styles from "./styles.module.scss";
-
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  return <input className={styles.input} {...props} />;
-};
