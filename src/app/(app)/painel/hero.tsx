866eb96783470f87438037a72d927e54c830aac2
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-13 23:00:57 -0300

    feat: added link

--- src/app/(app)/painel/hero.tsx
+++ src/app/(app)/painel/hero.tsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.scss";
 import hero from "@public/images/money.png";
 import { Button } from "@/components/forms/Button";
 import { Tag } from "@/components/Tag";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -10,7 +11,9 @@ export default function Hero() {
       <div className={styles.hero__box}>
         <Tag>NOVO JOGO</Tag>
         <h1>Estimule negociação com&nbsp;o Ganha-Grana!</h1>
-        <Button>Conheça o jogo</Button>
+        <Link href='/jogos/4' passHref>
+          <Button>Conheça o jogo</Button>
+        </Link>
       </div>
       <Image src={hero} alt='Imagem destacada' />
     </div>
