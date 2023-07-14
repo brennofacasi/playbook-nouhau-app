b898010b0d4c853ebdf47b22d890a9d66027bb6c
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-13 23:01:10 -0300

    feat: added recent games from api

--- src/app/(app)/painel/page.tsx
+++ src/app/(app)/painel/page.tsx
@@ -3,8 +3,20 @@ import styles from "./styles.module.scss";
 import Image from "next/image";
 import Hero from "./hero";
 import { Header } from "@/components/Header";
+import { fetchApi } from "@/services/fetch";
+import Link from "next/link";
+
+async function getGames() {
+  const res = await fetchApi("game");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
 
 export default async function Home() {
+  const games: GameCardProps[] = await getGames();
+
   return (
     <>
       <Header.Topbar />
@@ -13,26 +25,19 @@ export default async function Home() {
         <section className={styles.recent}>
           <h5>Jogos recentes</h5>
           <div className={styles.recent__row}>
-            <Image
-              width={960}
-              height={540}
-              src='/images/empresa@2x.png'
-              alt='Empresa de Participação'
-            />
-
-            <Image
-              width={960}
-              height={540}
-              src='/images/ganha@2x.png'
-              alt='Empresa de Participação'
-            />
-
-            <Image
-              width={960}
-              height={540}
-              src='/images/misterios@2x.png'
-              alt='Empresa de Participação'
-            />
+            {games
+              .map((game) => (
+                <Link key={game.id} href={`/jogos/${game.id}`}>
+                  <Image
+                    width={960}
+                    height={540}
+                    src={game.image}
+                    alt='Empresa de Participação'
+                  />
+                  <h6>{game.title}</h6>
+                </Link>
+              ))
+              .slice(0, 3)}
           </div>
         </section>
       </Card>
